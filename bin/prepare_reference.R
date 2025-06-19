0104aafccccb130730a41ed5c466a70b69946c63
Akito Shima <as82@sanger.ac.uk>
2025-06-19 16:11:08 +0100

    minor fix

--- bin/prepare_reference.R
+++ bin/prepare_reference.R
@@ -12,16 +12,16 @@ gtf_path <- args[1]
 sajr_path <- args[2]
 
 # Load SAJRsegments object
-seg <- SAJR::loadSAData(sajr_path)
+segments <- SAJR::loadSAData(sajr_path)
 
 # Annotate which segments are coding exons
-segs <- scsajr::add_is_coding_by_ens_gtf(gtf_path = gtf_path, segs) # input for func: segs or seg?
+segments <- scsajr::add_is_coding_by_ens_gtf(gtf_path = gtf_path, segments)
 
 # Assign segment types (e.g. ALT, EXN, INT)
-seg <- SAJR::setSplSiteTypes(seg, sajr_path)$seg
+segments <- SAJR::setSplSiteTypes(segments, sajr_path)$segments
 
 # Compute segment lengths
-seg$length <- seg$stop - seg$start + 1
+segments$length <- segments$stop - segments$start + 1
 
 # Load original GTF file
 gtf <- plotCoverage::loadEnsGTF(gtf_path)
@@ -32,7 +32,7 @@ if (is.null(gtf$transcript_name)) {
 # Flag which segments are identical to exons
 # Compares gene, start, and stop
 ef <- gtf$feature == "exon"
-seg$is_exon <- paste(seg$gene_id, seg$start, seg$stop) %in%
+segments$is_exon <- paste(segments$gene_id, segments$start, segments$stop) %in%
   paste(gtf$gene_id[ef], gtf$start[ef], gtf$stop[ef])
 
 # Build gene description data frame
